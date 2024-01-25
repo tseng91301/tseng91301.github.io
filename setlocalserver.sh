a872a500f18eb35807b0b688d710bf7e1c8da2f8
tseng91301 <tseng91301@gmail.com>
2024-01-25 16:53:24 +0800

    first commit

--- setlocalserver.sh
+++
@@ -1,2 +0,0 @@
-sudo mount -B ~/diskD_windowsData/remote_server/githubsite /var/www/html
-sudo service apache2 restart
--- transform.sh
+++
@@ -1,4 +0,0 @@
-git add index.html 
-git add site
-git commit -m "first commit"
-git push -u origin main
