7e56ad2724e90821e5daae5932f18317f3ce7465
tseng91301 <tseng91301@gmail.com>
2024-01-27 09:03:45 +0800

    first commit

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['ip'])){
+    $ip_addr = $_POST['ip'];
+    putenv("REMOTE_SERVER_IP=".$ip_addr);
+    http_response_code(200);
+}elseif($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $ip_addr = getenv("REMOTE_SERVER_IP");
+    echo("IP Address Now: ".$ip_addr);
+}
+?>
