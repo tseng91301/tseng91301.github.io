442b4a757bbe97f3b83f3831f4163052df0ca246
tseng91301 <tseng91301@gmail.com>
2025-05-20 09:07:37 +0800

    upload 404.html to redirect to homepage.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/404.html',
+          dest: '.'  // 複製到 dist/ 根目錄
+        }
+      ]
+    }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
