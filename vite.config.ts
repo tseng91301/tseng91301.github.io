f89b936fb4dd5cb8e7eddaec5e3138b929a859f4
Your Name <you@example.com>
2025-06-22 02:16:52 +0800

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',
   server: {
     host: "::",
     port: 8080,
